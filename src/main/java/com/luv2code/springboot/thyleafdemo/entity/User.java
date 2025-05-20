cfa8b59da9db7062f36ce76bfe2d7ac5728fbd15
DKhangggg <khangldse183186@fpt.edu.vn>
2025-05-20 23:03:26 +0700

    update login using spring securiy

---
+++ src/main/java/com/luv2code/springboot/thyleafdemo/entity/User.java
@@ -0,0 +1,67 @@
+package com.luv2code.springboot.thyleafdemo.entity;
+
+import jakarta.persistence.*;
+import org.springframework.context.annotation.Primary;
+
+import java.util.List;
+
+@Table(name = "users")
+@Entity
+public class User {
+    @Id
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+
+    private List<Role> roles;
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public User() {
+    }
+
+    public User(String username, String password, boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
